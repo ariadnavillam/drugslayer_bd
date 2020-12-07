c56b7738b194b2cc346b2cfd07a550db95327f8c
javiersanchezlorente <javiersanchezlorente@hotmail.com>
2020-12-07 22:32:34 +0100

    he añadido la funcion header() y lo he actualizado en todos los menus

--- menus.py
+++ menus.py
@@ -1,16 +1,17 @@
 from os import system
 
-def header(palabra):
-    print(" -----------------------------------------------------------------------------------")
-    print("|                                            %s                                     |" %palabra)
-    print(" -----------------------------------------------------------------------------------")
+def header(titulo):
+    long=80
+    long_palabra=len(titulo)
+    print(" " + "-" * long)
+    esp=(long-long_palabra)/2
+    print("|" + " " * int(esp) + titulo + " " * int(esp) + " |")
+    print(" " + "-" * long)
+
 
 def principal():
     system("cls")
-    print(" -----------------------------------------------------------------------------------")
-    print("|                                  BASE DE DATOS DE FARMACOS                        |")
-    print(" -----------------------------------------------------------------------------------")
-
+    header("BASE DE DATOS")
     print("Bienvenido a la base de datos 'disnet_drugslayer' del proyecto DISNET.")
     print("Cuenta con información sobre enfermedades a nivel fenotipico, biologico y fármacos.")
     print("\nPuede obtener diferentes tipos de información o funciones:")
@@ -26,24 +27,18 @@ def principal():
 
 def menu_1():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                        Información general                               | ")
-    print(" --------------------------------------------------------------------------")
+    header("Información general")
     print("En esta ventana puedes obtener varios tipos de información:")
     print("\n\t1. Número total")
     print("\n\t2. Primeras 10 instancias")
 
 def menu_1_1():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                        Información general                               | ")
-    print(" --------------------------------------------------------------------------")
+    header("Información general")
 
 def menu_1_2():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                        Información general                               | ")
-    print(" --------------------------------------------------------------------------")
+    header("Información general")
     print("En esta ventana puedes obtener las diez primeras instancias de:")
     print("\n\t1. Fármacos")
     print("\n\t2. Enfermedades")
@@ -53,9 +48,7 @@ def menu_1_2():
 
 def menu_2():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                     Información de los fármacos                          | ")
-    print(" --------------------------------------------------------------------------")
+    header("Información de los fármacos")
     print("En esta ventana puedes obtener varios tipos de información:")
     print("\n\ta. Información general del fármaco")
     print("\n\tb. Sinónimos de un fármaco")
@@ -63,18 +56,14 @@ def menu_2():
 
 def menu_3():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                   Información de las enfermedades                        | ")
-    print(" --------------------------------------------------------------------------")
+    header("Información de las enfermedades")
     print("En esta ventana puedes obtener información sobre las enfermedades:")
     print("\n\t1. Fármacos para una enfermedad")
     print("\n\t2. Fármaco y enfermedad con el mayor score de asociación")
 
 def menu_4():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                         Efectos fenotípicos                              | ")
-    print(" --------------------------------------------------------------------------")
+    header("Información de los efectos fenotípicos")
     print("En esta ventana podrá consultar información de los efectos fenotípicos \nasociados un fármaco.")
     print("Opciones: a. Indicaciones del fármaco: muestra los efectos fenotípicos \n"
           "             que sean indicaciones para las cuales se utiliza el fármaco.\n")
@@ -85,9 +74,7 @@ def menu_4():
 
 def menu_5():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                     Información de los targets                         | ")
-    print(" --------------------------------------------------------------------------")
+    header("Información de los targets")
     print("En esta ventana puedes obtener información sobre las dianas de un fármaco:")
     print("\n\ta. Dianas de un tipo dado:")
     print("\n\tb. Organismo al cual se asocian un mayor número de dianas")
@@ -95,25 +82,17 @@ def menu_5():
 
 def menu_6():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                                Borrados                                  | ")
-    print(" --------------------------------------------------------------------------")
+    header("Borrados")
     print("En esta ventana podrás Borrar asociación entre un fármaco y una enfermedad \n"
           "con un score muy bajo. En pantalla se muestran las 10 relaciones con un score\n "
           "más bajo. Escriba el nombre del fármaco y el nombre de la enfermedad separadas por un guión (-).")
 
 def menu_7():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                              Inserciones                                 | ")
-    print(" --------------------------------------------------------------------------")
+    header("Inserciones")
     print("En esta ventana podrás añadir una nueva enfermedad con su fármaco asociado")
 
 def menu_8():
     system("cls")
-    print(" --------------------------------------------------------------------------")
-    print("|                             Modificiones                                 | ")
-    print(" --------------------------------------------------------------------------")
+    header("Modificaciones")
     print("En esta ventana podrás establecer como 0 el score")
-
-
