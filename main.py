faa9c15af9453b30b9e8099f38504357e1f9ea6d
javiersanchezlorente <javiersanchezlorente@hotmail.com>
2020-12-07 14:19:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -68,11 +68,166 @@ finally:
     db.close()
     print("Data in file x.txt")
 
-#if opcion == 1:
+if int(opcion)==1:
+    system("cls")
+    print(" --------------------------------------------------------------------------")
+    print("|                        Información general                               | ")
+    print(" --------------------------------------------------------------------------")
+    print("En esta ventana puedes obtener varios tipos de información:")
+    print("\n\t1. Número total")
+    print("\n\t2. Primeras 10 instancias")
+
+    opcion2=input("\n¿Qué opción desea realizar? [Introduzca el número de la funcion deseada]")
+    try:
+        opcion2=int(opcion2)
+        if opcion2<1 or opcion2>2:
+            print("ERROR: Debe introducir un número entero del 1 al 2.")
+            exit()
+    except:
+        print("ERROR: Debe introducir un número entero del 1 al 2.")
+        exit()
+
+    if opcion2==1:
+        system("cls")
+        print(" --------------------------------------------------------------------------")
+        print("|                        Información general                               | ")
+        print(" --------------------------------------------------------------------------")
+        query1 = "SELECT COUNT(drug.drug_id) FROM drug"
+        cursor.execute(query1)
+        for row in cursor:
+            count_drug = row[0]
+            print("\n\t1. Número total de fármacos: %s" % count_drug)
+
+
+        query2 = "SELECT COUNT(resource_id) FROM disease"
+        cursor.execute(query2)
+        for row in cursor:
+            count_disease = row[0]
+            print("\n\t2. Número total de enfermedades: %s " % count_disease)
+
+
+        query3 = "SELECT COUNT(phenotype_id) FROM drug_phenotype_effect"
+        cursor.execute(query3)
+        for row in cursor:
+            count_phenotype = row[0]
+            print("\n\t3. Número total de efectos fenotípicos: %s " % count_phenotype)
+
+
+        query4 = "SELECT COUNT(target_id) FROM drug_target"
+        cursor.execute(query4)
+        for row in cursor:
+            count_target = row[0]
+            print("\n\t4. Número total de targets diferentes: %s " % count_target)
+
+        rep=input("\n¿Quiere hacer otra consulta? [S/N]")
+        if rep=="S":
+            system("python script1.py")
+        else:
+            exit()
+
+    if opcion2==2:
+        system("cls")
+        print(" --------------------------------------------------------------------------")
+        print("|                        Información general                               | ")
+        print(" --------------------------------------------------------------------------")
+        print("En esta ventana puedes obtener las diez primeras instancias de:")
+        print("\n\t1. Fármacos")
+        print("\n\t2. Enfermedades")
+        print("\n\t3. Efectos fenotipicos")
+        print("\n\t4. Dianas")
+
+        opcion3=input("\n¿Qué opción desea realizar? [Introduzca el número de la funcion deseada]")
+
+        try:
+            opcion3=int(opcion3)
+            if opcion3<1 or opcion3>4:
+                print("ERROR: Debe introducir un número entero del 1 al 2.")
+                exit()
+        except:
+            print("ERROR: Debe introducir un número entero del 1 al 2.")
+            exit()
+
+        if opcion3==1:
+            query1 = "SELECT drug_id, drug_name, molecular_type, chemical_structure, inchi_key FROM drug WHERE drug_id IS NOT NULL AND drug_name IS NOT NULL AND molecular_type IS NOT NULL AND chemical_structure IS NOT NULL AND inchi_key IS NOT NULL LIMIT 10"
+            cursor.execute(query1)
+            print("PRIMERAS 10 INSTANCIAS DE MEDICAMENTOS")
+            for row in cursor:
+                drug_id = row[0]
+                drug_name = row[1]
+                molecular_type = row[2]
+                chemical_structure = row[3]
+                inchi_key = row[4]
+                print(drug_id + "\t" + drug_name + "\t" + molecular_type + "\t" + chemical_structure + "\t" + inchi_key)
+
+        if opcion3==2:
+            query2 = "SELECT disease_id, disease_name FROM disease WHERE disease_id IS NOT NULL AND disease_name IS NOT NULL LIMIT 10"
+            cursor.execute(query2)
+            print("\nPRIMERAS 10 INSTANCIAS DE ENFERMEDADES")
+            for row in cursor:
+                disease_id = row[0]
+                disease_name = row[1]
+                print(disease_id + "\t" + disease_name)
+
+        if opcion3==3:
+            query3 = "SELECT phenotype_id, phenotype_name FROM phenotype_effect WHERE phenotype_id IS NOT NULL AND phenotype_name IS NOT NULL LIMIT 10"
+            cursor.execute(query3)
+            print("\nPRIMERAS 10 INSTANCIAS DE EFECTOS FENOTÍPICOS")
+            for row in cursor:
+                phenotype_id = row[0]
+                phenotype_name = row[1]
+                print(phenotype_id + "\t" + phenotype_name)
+
+        if opcion3==4:
+            query4 = "SELECT target_id, target_name_pref, target_type, target_organism FROM target WHERE target_id IS NOT NULL AND target_name_pref IS NOT NULL AND target_type IS NOT NULL AND target_organism IS NOT NULL LIMIT 10"
+            cursor.execute(query4)
+            print("\nPRIMERAS 10 INSTANCIAS DE TARGET")
+            for row in cursor:
+                target_id = row[0]
+                target_name = row[1]
+                target_type = row[2]
+                target_organism = row[3]
+                print(target_id + "\t" + target_name + "\t" + target_type + "\t" + target_organism)
 
 #elif opcion == 2:
 
-#elif opcion == 3:
+if opcion==3:
+    system("cls")
+    print(" --------------------------------------------------------------------------")
+    print("|                   Información de las enfermedades                        | ")
+    print(" --------------------------------------------------------------------------")
+    print("En esta ventana puedes obtener información sobre las enfermedades:")
+    print("\n\t1. Fármacos para una enfermedad")
+    print("\n\t2. Fármaco y enfermedad con el mayor score de asociación")
+
+    opcion2=input("\n¿Qué opción desea realizar? [Introduzca el número de la funcion deseada]")
+    try:
+        opcion2=int(opcion2)
+        if opcion2<1 or opcion2>2:
+            print("ERROR: Debe introducir un número entero del 1 al 2.")
+            exit()
+    except:
+        print("ERROR: Debe introducir un número entero del 1 al 2.")
+        exit()
+
+    if opcion2==1:
+        disease_name = input('Introduce el nombre de la enfermedad: ')
+
+        query1 = "SELECT drug.drug_id, drug.drug_name FROM drug, drug_disease, disease WHERE disease.disease_name=%s AND drug.drug_id=drug_disease.drug_id AND disease.disease_id=drug_disease.disease_id"
+        cursor.execute(query1, (disease_name,))
+        for row in cursor:
+            drug_id = row[0]
+            drug_name = row[1]
+            print("Drug_id: %s" % drug_id)
+            print("Drug_name: %s" % drug_name)
+
+    if opcion2==2:
+        query2 = "SELECT disease.disease_name, drug.drug_name FROM disease, drug, drug_disease WHERE drug.drug_id=drug_disease.drug_id AND disease.disease_id=drug_disease.disease_id ORDER BY drug_disease.inferred_score DESC LIMIT 1"
+        cursor.execute(query2)
+        for row in cursor:
+            disease_name = row[0]
+            drug_name = row[1]
+            print("Disease_name: %s" % disease_name)
+            print("Drug_name: %s" % drug_name)
 
 if int(opcion) == 4:
     system("cls")
