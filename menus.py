8f7629cf360ecdb6bb98f149f151e994f8fd6aec
javiersanchezlorente <javiersanchezlorente@hotmail.com>
2020-12-08 23:16:53 +0100

    quitado acentos y he añadido comentario para indicar que es script menu

--- menus.py
+++ menus.py
@@ -1,3 +1,5 @@
+# Script con los menus y submenus que se mostraran por pantalla
+
 from os import system
 
 def header(titulo):
@@ -13,13 +15,13 @@ def principal():
     system("cls")
     header("BASE DE DATOS")
     print("Bienvenido a la base de datos 'disnet_drugslayer' del proyecto DISNET.")
-    print("Cuenta con información sobre enfermedades a nivel fenotipico, biologico y fármacos.")
-    print("\nPuede obtener diferentes tipos de información o funciones:")
-    print("\n\t1. Información general")
-    print("\n\t2. Información de los fármacos")
-    print("\n\t3. Información de las enfermedades")
-    print("\n\t4. Información de los efectos fenotipicos")
-    print("\n\t5. Información de los targets")
+    print("Cuenta con informacion sobre enfermedades a nivel fenotipico, biologico y farmacos.")
+    print("\nPuede obtener diferentes tipos de informacion o funciones:")
+    print("\n\t1. Informacion general")
+    print("\n\t2. Informacion de los farmacos")
+    print("\n\t3. Informacion de las enfermedades")
+    print("\n\t4. Informacion de los efectos fenotipicos")
+    print("\n\t5. Informacion de los targets")
     print("\n\t6. Borrados")
     print("\n\t7. Inserciones")
     print("\n\t8. Modificaciones")
@@ -27,20 +29,20 @@ def principal():
 
 def menu_1():
     system("cls")
-    header("Información general")
-    print("En esta ventana puedes obtener varios tipos de información:")
-    print("\n\ta. Número total")
+    header("Informacion general")
+    print("En esta ventana puedes obtener varios tipos de informacion:")
+    print("\n\ta. Numero total")
     print("\n\tb. Primeras 10 instancias")
 
 def menu_1_1():
     system("cls")
-    header("Información general")
+    header("Informacion general")
 
 def menu_1_2():
     system("cls")
-    header("Información general")
+    header("Informacion general")
     print("En esta ventana puedes obtener las diez primeras instancias de:")
-    print("\n\ta. Fármacos")
+    print("\n\ta. Farmacos")
     print("\n\tb. Enfermedades")
     print("\n\tc. Efectos fenotipicos")
     print("\n\td. Dianas")
@@ -48,49 +50,49 @@ def menu_1_2():
 
 def menu_2():
     system("cls")
-    header("Información de los fármacos")
-    print("En esta ventana puedes obtener varios tipos de información:")
-    print("\n\ta. Información general del fármaco")
-    print("\n\tb. Sinónimos de un fármaco")
-    print("\n\tc. Código ATC de un fármaco")
+    header("Informacion de los farmacos")
+    print("En esta ventana puedes obtener varios tipos de informacion:")
+    print("\n\ta. Informacion general del farmaco")
+    print("\n\tb. Sinonimos de un farmaco")
+    print("\n\tc. Código ATC de un farmaco")
 
 def menu_3():
     system("cls")
-    header("Información de las enfermedades")
-    print("En esta ventana puedes obtener información sobre las enfermedades:")
-    print("\n\ta. Fármacos para una enfermedad")
-    print("\n\tb. Fármaco y enfermedad con el mayor score de asociación")
+    header("Informacion de las enfermedades")
+    print("En esta ventana puedes obtener informacion sobre las enfermedades:")
+    print("\n\ta. Farmacos para una enfermedad")
+    print("\n\tb. Farmaco y enfermedad con el mayor score de asociacion")
 
 def menu_4():
     system("cls")
-    header("Información de los efectos fenotípicos")
-    print("En esta ventana podrá consultar información de los efectos fenotípicos \nasociados un fármaco.")
-    print("Opciones: a. Indicaciones del fármaco: muestra los efectos fenotípicos \n"
-          "             que sean indicaciones para las cuales se utiliza el fármaco.\n")
+    header("Informacion de los efectos fenotipicos")
+    print("En esta ventana podra consultar información de los efectos fenotípicos \nasociados un farmaco:")
+    print("Opciones: a. Indicaciones del farmaco: muestra los efectos fenotipicos \n"
+          "             que sean indicaciones para las cuales se utiliza el farmaco.\n")
     print("          b. Efectos secundarios de un fármaco: muestra quellos efectos \n"
           "             fenotípicos categorizados como efectos secundarios generados\n"
-          "             por el fármaco ordenados de forma descendiente en base a la \n"
-          "             evidencia de esta asociación\n")
+          "             por el farmaco ordenados de forma descendiente en base a la \n"
+          "             evidencia de esta asociacion\n")
 
 def menu_5():
     system("cls")
-    header("Información de los targets")
-    print("En esta ventana puedes obtener información sobre las dianas de un fármaco:")
-    print("\n\ta. Dianas de un tipo dado:")
-    print("\n\tb. Organismo al cual se asocian un mayor número de dianas")
+    header("Informacion de los targets")
+    print("En esta ventana puedes obtener informacion sobre las dianas de un farmaco:")
+    print("\n\ta. Dianas de un tipo dado")
+    print("\n\tb. Organismo al cual se asocian un mayor numero de dianas")
 
 
 def menu_6():
     system("cls")
     header("Borrados")
-    print("En esta ventana podrás Borrar asociación entre un fármaco y una enfermedad \n"
+    print("En esta ventana podras borrar asociacion entre un farmaco y una enfermedad \n"
           "con un score muy bajo. En pantalla se muestran las 10 relaciones con un score\n "
-          "más bajo. Escriba el nombre del fármaco y el nombre de la enfermedad separadas por un guión (-).")
+          "mas bajo. Escriba el nombre del farmaco y el nombre de la enfermedad separadas por un guión (-).")
 
 def menu_7():
     system("cls")
     header("Inserciones")
-    print("En esta ventana podrás añadir una nueva enfermedad con su fármaco asociado")
+    print("En esta ventana podras añadir una nueva enfermedad con su farmaco asociado")
 
 def menu_8():
     system("cls")
@@ -100,7 +102,5 @@ def menu_8():
 def final(database):
     header("Adios")
     print("Sus consultas se han guardado en el archivo 'Resultados.txt'")
-    database.close()       
+    database.close()
     exit()
-
-
