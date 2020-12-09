9fb5dcb55437cd4e033d0437d534e27f39cbc7ed
olgahs <67432163+olgahs@users.noreply.github.com>
2020-12-09 13:37:26 +0100

    Update consulta3.py

--- consulta3.py
+++ consulta3.py
@@ -16,14 +16,14 @@ cursor = db.cursor()
 
 try:
     disease_name = input('Introduce el nombre de la enfermedad: ')
-    
+    print("Id del fármaco\tNombre del fármaco")
     query1 = "SELECT drug.drug_id, drug.drug_name FROM drug, drug_disease, disease WHERE disease.disease_name=%s AND drug.drug_id=drug_disease.drug_id AND disease.disease_id=drug_disease.disease_id"
     cursor.execute(query1, (disease_name,))
     for row in cursor:
         drug_id = row[0]
         drug_name = row[1]
-        print("Drug_id: %s" % drug_id)
-        print("Drug_name: %s" % drug_name)
+        
+        print(drug_id + "\t" + drug_name)
         
 except ValueError:
     print("Error: el nombre de la enfermedad no existe")
@@ -50,7 +50,7 @@ try:
     for row in cursor:
         disease_name = row[0]
         drug_name = row[1]
-        print("Disease_name: %s" % drug_id)
+        print("Disease_name: %s" % disease_name)
         print("Drug_name: %s" % drug_name)
         
 except ValueError:
